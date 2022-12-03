233654fb00037bcacd12667f495c8a26559da299
Aurelien Kermabon <aurelien.kermabon@doctolib.com>
2022-12-03 10:46:39 +0100

    refacto

---
+++ 01/main.rb
@@ -0,0 +1,22 @@
+rows = File.read(Dir.pwd + '/01/input.txt').split("\n\n")
+
+def all_sorted_calories(rows)
+  rows
+    .map { |one_elf| one_elf.split("\n").map(&:to_i).sum }
+    .sort
+    .reverse
+end
+
+def max_calories(rows)
+  all_sorted_calories(rows).first
+end
+
+def top3_sum_calories(rows)
+  all_sorted_calories(rows).take(3).sum
+end
+
+# part 1
+p max_calories(rows)
+
+# part 2
+p top3_sum_calories(rows)
