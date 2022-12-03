3dfbdff82386a535b1e70a645d86361ed4e06d8a
Aurelien Kermabon <aurelien.kermabon@doctolib.com>
2022-12-03 10:52:08 +0100

    refacto

--- 03/main.rb
+++ 03/main.rb
@@ -1,10 +1,11 @@
+require 'pry'
 items = File.read(Dir.pwd + '/03/input.txt').split("\n")
 
 SCORES = ('a'..'z').each_with_index.each_with_object({}) { |(letter, index), hash| hash[letter] = index + 1 }
 ('A'..'Z').each_with_index { |letter, index| SCORES[letter] = index + 27 }
 
 def common_item(compartments)
-  (compartments[0] & compartments[1]).first
+  compartments.reduce { |common, compartment| common & compartment }.first
 end
 
 def stacks(items)
@@ -16,16 +17,12 @@ def compartments(stack)
   [stack.take(compartment_size), stack.last(compartment_size)]
 end
 
-def badge(stacks)
-  (stacks[0] & stacks[1] & stacks[2]).first
-end
-
 def sum_priorities_common_item(items)
   stacks(items).map { |stack| SCORES[common_item(compartments(stack))] }.sum
 end
 
 def sum_badge_priorities(items)
-  stacks(items).each_slice(3).map { |group_stacks| SCORES[badge(group_stacks)] }.sum
+  stacks(items).each_slice(3).map { |group_stacks| SCORES[common_item(group_stacks)] }.sum
 end
 
 # part 1
