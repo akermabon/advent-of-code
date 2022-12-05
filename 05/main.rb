900ede59c474d6a56bb350e2d5dfcf90bc196d14
Aurelien Kermabon <aurelien.kermabon@doctolib.com>
2022-12-05 13:43:13 +0100

    refacto

--- 05/main.rb
+++ 05/main.rb
@@ -6,20 +6,16 @@ def parse_crates(string)
   rows = string.split("\n")
   crates = 9.times.each_with_object([]) { |n, hash| hash[n] = [] }
   rows.each do |row|
-    row.split('').each_slice(4).with_index do |group, col|
-      crates[col].unshift(group[1]) if /[A-Z]/.match(group[1])
-    end
+    row.split('').each_slice(4).with_index { |group, col| crates[col].unshift(group[1]) if /[A-Z]/.match(group[1]) }
   end
   crates
 end
 
 def parse_instructions(string)
-  string
-    .split("\n")
-    .map do |row|
-      nb_moves, from, to = /move (\d+) from (\d+) to (\d+)/.match(row).to_a.last(3).map(&:to_i)
-      { nb_moves: nb_moves, from: from - 1, to: to - 1 }
-    end
+  string.split("\n").map do |row|
+    nb_moves, from, to = /move (\d+) from (\d+) to (\d+)/.match(row).captures.map(&:to_i)
+    { nb_moves: nb_moves, from: from - 1, to: to - 1 }
+  end
 end
 
 def move(crates, nb_moves, from, to)
@@ -35,9 +31,8 @@ def move_with_3001(crates, todo)
   move(crates, todo[:nb_moves], todo[:from], todo[:to])
 end
 
-def top_with(crates, instructions, cratemover)
-  method = 'move_with_' + cratemover
-  instructions.each { |todo| send(method, crates, todo) }
+def top_with(crates, instructions, move_method)
+  instructions.each { |todo| send(move_method, crates, todo) }
   crates.map { |stack| stack.last }.join
 end
 
@@ -49,7 +44,7 @@ crates = parse_crates(crates_string)
 instructions = parse_instructions(instructions_string)
 
 # part 1
-p top_with(copy(crates), instructions, '3000')
+p top_with(copy(crates), instructions, :move_with_3000)
 
 # part 2
-p top_with(copy(crates), instructions, '3001')
+p top_with(copy(crates), instructions, :move_with_3001)
