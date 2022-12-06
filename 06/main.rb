216ff178e2b4bc7ccac06c8d09d3dbb431d3b79b
Aurelien Kermabon <aurelien.kermabon@doctolib.com>
2022-12-06 12:03:20 +0100

    day 6

---
+++ 06/main.rb
@@ -0,0 +1,20 @@
+packet = File.read(Dir.pwd + '/06/input.txt')
+
+def is_marker?(string)
+  splitted = string.split('')
+  splitted.size == splitted.uniq.size
+end
+
+def find_first_marker(packet, marker_size)
+  packet.split('').each_with_index.find_index do |_, index|
+    next false if index < (marker_size - 1)
+    substring = packet.slice(index - (marker_size - 1)..index)
+    is_marker?(substring)
+  end + 1
+end
+
+# part 1
+p find_first_marker(packet, 4)
+
+# part 2
+p find_first_marker(packet, 14)
